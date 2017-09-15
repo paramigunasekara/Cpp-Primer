15853e9790868e04fd044041cb182017171631e7
Parami Gunasekara <parami@live.ca>
2017-09-15 12:01:04 -0400

    testing 3.8

--- ch03/ex3_8.cpp
+++ ch03/ex3_8.cpp
@@ -14,8 +14,8 @@ int main()
     cout << str << endl;
 
     // for
-    for (i = 0; i<str.size(); ++i) str[i] = 'Y';
-    cout << str << endl;
+    for (i = 0; i<str.size(); i++) cout << i << endl;
+    //cout << i << endl;
 
     // I like range for.
     return 0;
